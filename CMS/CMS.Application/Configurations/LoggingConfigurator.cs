956ba3c959a613529877b9eaa1e2d209d7ca0a87
Mousa.AlMajthoob <Mousa.AlMajthoob@MOUSA>
2025-01-22 20:14:03 +0300

    .

--- CMS/CMS.Application/Configurations/LoggingConfigurator.cs
+++ CMS/CMS.Application/Configurations/LoggingConfigurator.cs
@@ -11,12 +11,10 @@ public static class LoggingConfigurator
 {
     public static IHostBuilder SetupConfiguration(this IHostBuilder hostBuilder, string projectName)
     {
-        string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
         IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
                                                                      .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                                                                      .AddJsonFile(Path.Combine("LogSettings", "appsettings.LogsConfig.json"), optional: true, reloadOnChange: true)
-                                                                     .AddJsonFile(Path.Combine("LogSettings", $"appsettings.LogsConfig.{environment}.json"), optional: true, reloadOnChange: true)
+                                                                     .AddJsonFile(Path.Combine("LogSettings", $"appsettings.LogsConfig.Development.json"), optional: true, reloadOnChange: true)
                                                                      .Build();
 
         Log.Logger = new LoggerConfiguration()
